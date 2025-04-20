14d8dbfb7d117bf69986547df10eb175385cb94c
noshishiRust <nopenoshishi@gmail.com>
2025-04-20 10:54:52 +0900

    5-3 section driver

--- crates/main/src/main.rs
+++ crates/main/src/main.rs
@@ -5,22 +5,22 @@ use panic_halt as _;
 use wio_terminal as wio;
 use wio::entry;
 use wio::pac::Peripherals;
-use wio::prelude::*;
+
+use driver::{Button1, Led};
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let pins = wio::Pins::new(peripherals.PORT).split();
 
-    let mut user_led = pins.user_led.into_push_pull_output();
-    let button1 = pins.buttons.button1.into_floating_input();
-
+    let mut led = Led::new(pins.user_led);
+    let mut button1 = Button1::new(pins.buttons.button1);
 
     loop {
-        if button1.is_high().unwrap() {
-            user_led.set_high().unwrap();
+        if button1.is_pressed() {
+            led.turn_on();
         } else {
-            user_led.set_low().unwrap();
+            led.turn_off();
         }
     }
 }
